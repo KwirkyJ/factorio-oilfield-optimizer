5d9febcc754af4d8efbce6f18797e55d1cc86e56
KwirkyJ <kwirkyj.smith0@gmail.com>
2017-04-02 11:36:32 -0500

    update moretables version

--- lib/moretables.lua
+++ lib/moretables.lua
@@ -1,21 +1,21 @@
 -- Utility library to support useful operations on tables not present in the 
 -- standard library.
 -- 
--- Author:   J. 'KwirkyJ' Smith <kwirkyj.smith0@gmail.com>
--- Date:     2016
--- Version:  1.1.0
--- License:  MIT (X11) License
+-- Author:  J. 'KwirkyJ' Smith <kwirkyj.smith0@gmail.com>
+-- Date:    2016
+-- Version: 1.2.0
+-- License: MIT (X11) License
 -- Homepage: https://github.com/KwirkyJ/moretables
 
 
 
-local Buffer = require 'lib.lua_stringbuffer'
+local Buffer = require 'lua_stringbuffer'
 
 
 
 local DEFAULT_DELTA = 1e-12
 
-local t = {_VERSION = "1.1.0",
+local T = {_VERSION = "1.2.0",
            _delta   = DEFAULT_DELTA,
           }
 
@@ -27,7 +27,7 @@ local t = {_VERSION = "1.1.0",
 local getTolerance = function(self)
     return self._delta
 end
-t.getTolerance = getTolerance
+T.getTolerance = getTolerance
 
 ---moretables:setTolerance(delta)
 -- Set the delta/tolerance used in tables.alike when none is supplied.
@@ -38,7 +38,7 @@ local setTolerance = function(self, delta)
     assert(type(delta) == 'number', 'delta argument must be a number')
     self._delta = delta
 end
-t.setTolerance = setTolerance
+T.setTolerance = setTolerance
 
 
 
@@ -47,7 +47,7 @@ t.setTolerance = setTolerance
 -- @param t The table.
 -- @return nil and failure message iff not a table;
 --         else the count of elements in the table.
-len = function(t)
+local len = function(t)
  if type(t) ~= 'table' then 
         return nil, "Table expected, received "..type(t)
     end
@@ -57,7 +57,7 @@ len = function(t)
     end
     return count
 end
-t.len = len
+T.len = len
 
 ---moretables.alike(t1, t2[, maxdelta] [, use_mt] [, loc])
 -- Check whether two tables are equivalent;
@@ -70,15 +70,19 @@ t.len = len
 -- @param loc      Internal string to place mismatched elements.
 -- @return True iff tables share same keys and respective values differ by 
 --         less than maxdelta; else false and failure message.tab1
+local alike
 alike = function(t1, t2, maxdelta, use_mt, loc)
     -- the code would be much more elegant if it was not as concerned with
     -- providing error messages.
-    maxdelta = maxdelta or t._delta
+    maxdelta = maxdelta or T._delta
     local preface = ''
     if type(t1) ~= type(t2) then 
         preface = 'Differing types'
-        if loc then preface = 'Differing types at ' .. loc end
-        return false,string.format('%s: %s ~= %s', preface, type(t1), type(t2))
+        if loc then 
+            preface = string.format ('Differing types at %s', tostring (loc)) 
+        end
+        return false, string.format('%s: %s ~= %s', 
+                                    preface, type(t1), type(t2))
     elseif type(t1) == 'number' then
         if (math.max(t1,t2) - math.min(t1,t2) > maxdelta) then
             if loc then
@@ -97,6 +101,7 @@ alike = function(t1, t2, maxdelta, use_mt, loc)
             if loc then preface = 'Tables unequal at: ' .. loc end
             return false, preface
         end
+        
         local len1, len2 = len(t1), len(t2)
         if len1 ~= len2 then
             preface = 'Tables of differing length'
@@ -111,8 +116,10 @@ alike = function(t1, t2, maxdelta, use_mt, loc)
         for k, v1 in pairs(t1) do
             if type(k) == 'string' then 
                 key = string.format("%s['%s']", loc, k)
-            else
+            elseif type(k) == 'number' then
                 key = string.format("%s[%d]", loc, k)
+            else 
+                key = string.format ("%s[%s]", loc, tostring (k))
             end
             local ok, err = alike(v1, t2[k], maxdelta, use_mt, key)
             if not ok then return ok, err end
@@ -128,7 +135,7 @@ alike = function(t1, t2, maxdelta, use_mt, loc)
     end
     return true
 end
-t.alike = alike
+T.alike = alike
 
 
 ---Create a deep copy of a table
@@ -152,7 +159,7 @@ clone = function (t)
     end
     return c
 end
-t.clone = clone
+T.clone = clone
 
 
 
@@ -203,7 +210,7 @@ local getOrderedKeys = function(t, comp, filter)
     table.sort(list, comp)
     return list
 end
-t.getOrderedKeys = getOrderedKeys
+T.getOrderedKeys = getOrderedKeys
 
 
 
@@ -213,6 +220,7 @@ t.getOrderedKeys = getOrderedKeys
 -- @param indent Number for the number of spaces to indent (default 0).
 -- @return Iff type(t) ~= 'table' then tostring(t);
 --         else, pretty-prints the table and any nested contents.
+local toStr
 toStr = function(t, indent)
     if type(t) ~= 'table' then return tostring(t) end
     
@@ -223,7 +231,8 @@ toStr = function(t, indent)
     if #indices == 0 then return '{}' end -- empty table
     
     indent = indent or 0
-    local ins, value = string.rep(' ', indent+2), nil
+    local ins, value
+    ins = string.rep(' ', indent+2)
     sb:add('{')
     for i=1, #indices do
         key = indices[i]
@@ -244,9 +253,9 @@ toStr = function(t, indent)
     sb:add(string.rep(' ', indent) .. '}')
     return sb:getString()
 end
-t.tostring = toStr
+T.tostring = toStr
 
 
 
-return t
+return T
 
